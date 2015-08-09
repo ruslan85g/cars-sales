7db64b452cbc70b7c709514b5e8f0e94187f00a0
ruslan85g <ruslan85g@gmail.com>
2015-08-09 21:10:09 +0300

    okkasq1

--- src/main/java/com/shankar/cars/action/CarsServlet.java
+++ src/main/java/com/shankar/cars/action/CarsServlet.java
@@ -15,8 +15,9 @@ import javax.ws.rs.core.Response;
 import lombok.extern.java.Log;
 
 import com.shankar.cars.data.Car;
-import com.shankar.cars.data.User;
+import com.shankar.cars.data.CarType;
 import com.shankar.cars.data.meta.CarMeta;
+import com.shankar.cars.data.meta.CarTypeMeta;
 import com.shankar.cars.data.persist.CarDBService;
 
 @Path("/cars")
@@ -26,7 +27,7 @@ public class CarsServlet {
 	HttpServletRequest request;
 	@Context
 	HttpServletResponse response;
-	
+
 	CarDBService carDBService = new CarDBService();
 
 	@Path("/get/{car_id}")
@@ -36,10 +37,11 @@ public class CarsServlet {
 	public CarMeta newApplication(@QueryParam("car_id") Long car_id) {
 		log.info("Start newApplication ");
 
-	/*	CarDBService db = new CarDBService();
-		Car car = db.load(Car.class, car_id);
-		CarMeta carMeta = new CarMeta();
-		carMeta.setCar_id(car.getCar_id());*/
+		/*
+		 * CarDBService db = new CarDBService(); Car car = db.load(Car.class,
+		 * car_id); CarMeta carMeta = new CarMeta();
+		 * carMeta.setCar_id(car.getCar_id());
+		 */
 		CarMeta carMeta = new CarMeta();
 		carMeta.setCar_id(1L);
 		/*
@@ -65,35 +67,66 @@ public class CarsServlet {
 	public Response newApplication(CarMeta carMeta) {
 
 		log.info("Start newApplication ");
-	
+
 		Car car = null;
-		
-		
-		/*if(user == null ){
-			Response.serverError().status(Response.Status.BAD_REQUEST).entity( "User not found" ).build();
-		}*/
-		
-		try{ 
-		
-		if(carMeta.getCar_id() != null){
-		  car = carDBService.load(Car.class, carMeta.getCar_id() );
-		  car.setUpdate_time(System.currentTimeMillis());
-		}else{
-		  car = new Car();
-		  car.setCreated_time(System.currentTimeMillis());
+
+		/*
+		 * if(user == null ){
+		 * Response.serverError().status(Response.Status.BAD_REQUEST).entity(
+		 * "User not found" ).build(); }
+		 */
+
+		try {
+
+			if (carMeta.getCar_id() != null) {
+				car = carDBService.load(Car.class, carMeta.getCar_id());
+				car.setUpdate_time(System.currentTimeMillis());
+			} else {
+				car = new Car();
+				car.setCreated_time(System.currentTimeMillis());
+			}
+
+			car.setCar_model(carMeta.getCar_model());
+			car.setCar_name(carMeta.getCar_name());
+			car.setCar_url(carMeta.getCar_url());
+
+			carDBService.save(car);
+
+		} catch (Exception e) {
+			log.severe("Exception::" + e.getMessage());
+			Response.serverError().build();
 		}
-		
-		car.setCar_model(carMeta.getCar_model());
-		car.setCar_name(carMeta.getCar_name());
-		car.setCar_url(carMeta.getCar_url());
-		
-		carDBService.save(car);
-	
-		}catch(Exception e){
-			 log.severe("Exception::" + e.getMessage());
-			 Response.serverError().build();
-		}
-		
+
+		log.info("End newApplication");
+		return Response.ok().build();
+	}
+
+	@Path("/saveCarTypes/{carType}")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response newApplication(CarType carType) {
+		log.info("Start newApplication ");
+
+		/*
+		 * CarDBService db = new CarDBService(); Car car = db.load(Car.class,
+		 * car_id); CarMeta carMeta = new CarMeta();
+		 * carMeta.setCar_id(car.getCar_id());
+		 */
+		CarTypeMeta carTypeMeta = new CarTypeMeta();
+		carTypeMeta.setManufature_id(1l);
+		/*
+		 * Response resp = null;
+		 * 
+		 * try{ ApplicationLogic al = new ApplicationLogic(); Boolean flag =
+		 * al.saveApplication( appMeta );
+		 * 
+		 * if(flag){ resp = Response.ok().build(); }else{ resp =
+		 * Response.notModified().build(); }
+		 * 
+		 * }catch(Exception e){ log.info("Exception::" + e.getMessage()); resp =
+		 * Response.serverError().build(); }
+		 */
 
 		log.info("End newApplication");
 		return Response.ok().build();
