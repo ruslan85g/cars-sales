91370ee108780f292d0582799cbf16be60e79c1f
ruslan85g <ruslan85g@gmail.com>
2015-08-26 22:31:01 +0300

    sasf

---
+++ src/main/java/com/shankar/cars/data/meta/ModelList.java
@@ -0,0 +1,17 @@
+package com.shankar.cars.data.meta;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class ModelList {
+
+	// "id" : 3,S
+	// "name" : "A1"
+	@Getter
+	@Setter
+	Long modelId;
+
+	@Getter
+	@Setter
+	String modelName;
+}
