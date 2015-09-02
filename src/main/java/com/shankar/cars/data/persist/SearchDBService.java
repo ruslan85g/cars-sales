b2e5aa309e4a38302739d348ba82e6a01086ee1f
ruslan85g <ruslan85g@gmail.com>
2015-09-02 20:43:51 +0300

    repair query

--- src/main/java/com/shankar/cars/data/persist/SearchDBService.java
+++ src/main/java/com/shankar/cars/data/persist/SearchDBService.java
@@ -4,7 +4,7 @@ import static com.shankar.cars.data.persist.OfyService.ofy;
 
 import java.util.List;
 
-import com.googlecode.objectify.cmd.Query;
+import com.googlecode.objectify.cmd.LoadType;
 import com.shankar.cars.data.Car;
 import com.shankar.cars.data.meta.SearchMeta;
 
@@ -16,40 +16,39 @@ public class SearchDBService extends DBService {
 		if (searchMeta == null) {
 			throw new RuntimeException("searchMeta cannot be null");
 		}
-		List<Car> cars = (List<Car>) ofy().load().type(class1);
+		// List<Car> cars = (List<Car>) ofy().load().type(class1);
+		LoadType<Car> cars = ofy().load().type(class1);
 		if (searchMeta.getCar_type_id() != null) {
-			((Query<Car>) cars).filter("car_type_id ==",
-					searchMeta.getCar_type_id());
+			cars.filter("car_type_id ==", searchMeta.getCar_type_id());
 		}
 		if (searchMeta.getCar_model_id() != null) {
-			((Query<Car>) cars).filter("car_model_id ==",
-					searchMeta.getCar_model_id());
+
+			cars.filter("car_model_id ==", searchMeta.getCar_model_id());
 		}
 		if (searchMeta.getYearF() != null) {
-			((Query<Car>) cars).filter("yearF", searchMeta.getYearF());
+			cars.filter("yearF", searchMeta.getYearF());
 		}
 		if (searchMeta.getType_geare() != null
 				&& !searchMeta.getType_geare().isEmpty()) {
-			((Query<Car>) cars)
-					.filter("type_geare", searchMeta.getType_geare());
+			cars.filter("type_geare", searchMeta.getType_geare());
 		}
 		if (searchMeta.getVolume() != null && !searchMeta.getVolume().isEmpty()) {
-			((Query<Car>) cars).filter("volume", searchMeta.getVolume());
+			cars.filter("volume", searchMeta.getVolume());
 		}
 		if (searchMeta.getKm() != null) {
-			((Query<Car>) cars).filter("km", searchMeta.getKm());
+			cars.filter("km", searchMeta.getKm());
 		}
 		if (searchMeta.getColor() != null && !searchMeta.getColor().isEmpty()) {
-			((Query<Car>) cars).filter("color", searchMeta.getColor());
+			cars.filter("color", searchMeta.getColor());
 		}
 		if (searchMeta.getPriceF() != null) {
-			((Query<Car>) cars).filter("priceF", searchMeta.getPriceF());
+			cars.filter("priceF", searchMeta.getPriceF());
 		}
 		if (searchMeta.getText() != null && !searchMeta.getText().isEmpty()) {
-			((Query<Car>) cars).filter("text", searchMeta.getText());
+			cars.filter("text", searchMeta.getText());
 		}
-		((Query<Car>) cars).list();
-		return cars;
+		cars.list();
+		return (List<Car>) cars;
 	}
 
 	// public <E> List<E> load(Class<E> type, String feild, String value)
