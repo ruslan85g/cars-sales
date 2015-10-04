44ca8b0e61d19df113da2ab1fd14bc1ef083b605
maria-brook <mariya.brook@gmail.com>
2015-10-04 17:37:59 +0000

    UI

--- src/main/webapp/javascript/controllers/app.js
+++ src/main/webapp/javascript/controllers/app.js
@@ -1,15 +1,11 @@
 function MainCntl($scope,$http, $rootScope, $location, $route,$cookieStore) {
 	userOperations($scope,$http, $rootScope, $location, $route,$cookieStore);
 	popupFunctions($scope,$http, $rootScope, $location, $route,$cookieStore);
-	/*$scope.routAcount = function(){
-		$location.url = '/account';
-	}*/
-	//if($cookieStore.get('tab')){console.log('0000000000000')}else{console.log('11111111111111')}
-	// $scope.lastVal = $cookieStore.get('tab');
+
 	$scope.userID = "string";
 	console.log($cookieStore)
 	$scope.years = [1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015];
-	$scope.man_opts = [{"id":1,"name":"aaa"},{"id":2,"name":"bbb"},{"id":3,"name":"ccc"}];
+	$scope.man_opts = [];
 	
 	$scope.getListModel = function(id){
 	console.log(id)
@@ -17,16 +13,25 @@ function MainCntl($scope,$http, $rootScope, $location, $route,$cookieStore) {
 			$.each($scope.man_opts, function (key,val){
 				if(val.name == id){$scope.typeId = val.id}
 			});
-			$http.get(''+$rootScope.mainurl+'/api/carmodels/get',{ "id"   :  $scope.typeId }).
+			$scope.listModel = {"car_type_id" : $scope.typeId,"car_type_name":""};
+			$http.post(''+$rootScope.mainurl+'/api/carmodels/get',$scope.listModel).
 			success(function(data, status) {
 				console.log(data);
+				$.each(data, function (key,val){
+					var tmp = {"id":val.car_type_id,"name":val.car_type_name};
+					$scope.mod_opts.push(tmp)
+				});
 			}).error(function(data, status) {console.log(data);});
 		}
 	}
-	
+	$scope.man_opts = [];
 	$http.get(''+$rootScope.mainurl+'/api/cartypes/getalltypes').
 		success(function(data, status) {
 			console.log(data);
+			$.each(data, function (key,val){
+				var tmp = {"id":val.car_type_id,"name":val.car_type_name};
+				$scope.man_opts.push(tmp)
+			});
 		}).error(function(data, status) {console.log(data);});
 	
 	$http.get(''+$rootScope.mainurl+'/api/home', {}).
