42630c598d353869a265a813942405f09f2c4598
ruslan85g <ruslan85g@gmail.com>
2015-11-22 22:53:21 +0200

    qerfrfv12

--- src/main/java/com/shankar/cars/action/SearchServlet.java
+++ src/main/java/com/shankar/cars/action/SearchServlet.java
@@ -47,6 +47,81 @@ public class SearchServlet {
 		log.info("Start search ");
 
 		List<SearchResponseMeta> searchResponseMetaList = new ArrayList<>();
+//		if (searchMeta.getCar_model_id() == null
+//				&& searchMeta.getCar_type_id() == null
+//				&& searchMeta.getPriceF() == null
+//				&& searchMeta.getPriceT() == null
+//				&& searchMeta.getType_geare() == null
+//				&& searchMeta.getVolume() == null
+//				&& searchMeta.getYearF() == null
+//				&& searchMeta.getYearT() != null) {
+//			log.info("Start search per YearT");
+//
+//			List<Car> cars = searchDBService
+//					.loadPerYearT(Car.class, searchMeta);
+//			log.info("find : " + cars.size() + "cars");
+//			UserDBService db = new UserDBService();
+//			CarModel carModel = null;
+//			for (Car car : cars) {
+//				SearchResponseMeta searchResponseMeta = new SearchResponseMeta();
+//				if (car.getCar_id() != null) {
+//					searchResponseMeta.setCar_id(car.getCar_id());
+//					log.info("Start setCar_id ");
+//				}
+//				if (car.getCar_model_id() != null) {
+//
+//					carModel = carModelsDBService.load(CarModel.class,
+//							car.getCar_model_id());
+//					if (carModel != null) {
+//						searchResponseMeta.setCar_model_name(carModel
+//								.getModel_name());
+//					}
+//				}
+//				if (car.getCar_type_id() != null) {
+//					CarType carType = carTypeDBService.load(CarType.class,
+//							car.getCar_type_id());
+//					if (carType != null) {
+//						searchResponseMeta.setCar_type_name(carType
+//								.getCar_type_name());
+//					}
+//
+//				}
+//				if (car.getCar_id() != null) {
+//					searchResponseMeta.setCar_id(car.getCar_id());
+//				} else {
+//					String car_url = car.getUser_id().toString()
+//							+ carModel.getModel_name();
+//					log.info("try find Car per Car_id ");
+//					Car newCar = db.loadOne(Car.class, "car_url", car_url);
+//					if (newCar != null) {
+//						log.info(" find Car SUCSESS");
+//						searchResponseMeta.setCar_id(newCar.getCar_id());
+//						searchResponseMeta.setImage(newCar.getImage());
+//					} else {
+//						log.info(" find Car FAILED");
+//					}
+//				}
+//				searchResponseMeta.setColor(car.getColor());
+//				// searchResponseMeta.setCar_id(car.getCar_id());
+//				searchResponseMeta.setYear(car.getYear());
+//				searchResponseMeta.setKm(car.getKm());
+//				searchResponseMeta.setPrice(car.getPrice());
+//				searchResponseMeta.setType_geare(car.getType_geare());
+//				searchResponseMeta.setVolume(car.getVolume());
+//				User user = db.load(User.class, car.getUser_id());
+//				searchResponseMeta.setUser(user);
+//				if (car.getImage() != null) {
+//					log.info("Start set Car Photo");
+//					searchResponseMeta.setImage(car.getImage());
+//					log.info(" set Car Photo SUCSESS");
+//				}
+//				searchResponseMetaList.add(searchResponseMeta);
+//			}
+//
+//			log.info("End search Per YearT");
+//			return searchResponseMetaList;
+//
+//		}
 		List<Car> cars = searchDBService.load(Car.class, searchMeta);
 		log.info("find : " + cars.size() + "cars");
 		UserDBService db = new UserDBService();
