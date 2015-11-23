4f85c2c5811d11d73969445018b6f68a779a2857
ruslan85g <ruslan85g@gmail.com>
2015-11-23 15:42:20 +0200

    2rfrf12

--- src/main/java/com/shankar/cars/action/CarsServlet.java
+++ src/main/java/com/shankar/cars/action/CarsServlet.java
@@ -22,9 +22,11 @@ import javax.ws.rs.core.Response;
 import lombok.extern.java.Log;
 
 import com.shankar.cars.data.Car;
+import com.shankar.cars.data.CarModel;
 import com.shankar.cars.data.CarType;
 import com.shankar.cars.data.User;
 import com.shankar.cars.data.meta.CarMeta;
+import com.shankar.cars.data.meta.SearchResponseMeta;
 import com.shankar.cars.data.meta.UserMeta;
 import com.shankar.cars.data.persist.CarDBService;
 import com.shankar.cars.data.persist.CarModelsDBService;
@@ -231,20 +233,78 @@ public class CarsServlet {
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public List<Car> getAllCars() throws Exception {
+	public List<SearchResponseMeta>  getAllCars() throws Exception {
 		log.info("Start newApplication ");
-		List<Car> carMetas = new ArrayList<>();
 		CarDBService cardb = new CarDBService();
+		List<SearchResponseMeta> searchResponseMetaList = new ArrayList<>();
+		List<Car> cars = cardb.loadAll(Car.class);
+		log.info("find : " + cars.size() + "cars");
+		UserDBService db = new UserDBService();
+		CarModel carModel = null;
+		for (Car car : cars) {
+			SearchResponseMeta searchResponseMeta = new SearchResponseMeta();
+			if (car.getCar_id() != null) {
+				searchResponseMeta.setCar_id(car.getCar_id());
+				log.info("Start setCar_id ");
+			}
+			if (car.getCar_model_id() != null) {
 
-		carMetas = cardb.loadAll(Car.class);
+				carModel = carModelsDBService.load(CarModel.class,
+						car.getCar_model_id());
+				if (carModel != null) {
+					searchResponseMeta.setCar_model_name(carModel
+							.getModel_name());
+				}
+			}
+			if (car.getCar_type_id() != null) {
+				CarType carType = carTypeDBService.load(CarType.class,
+						car.getCar_type_id());
+				if (carType != null) {
+					searchResponseMeta.setCar_type_name(carType
+							.getCar_type_name());
+				}
 
-		if (carMetas == null) {
-			log.info("CarsNotFouds ");
-			throw new Exception("CarsNotFouds");
-
+			}
+			if (car.getCar_id() != null) {
+				searchResponseMeta.setCar_id(car.getCar_id());
+			} else {
+				String car_url = car.getUser_id().toString()
+						+ carModel.getCar_model_id().toString()
+						+ car.getCar_type_id().toString();
+				log.info("try find Car per Car_id ");
+				Car newCar = carDBService
+						.loadOne(Car.class, "car_url", car_url);
+				if (newCar.getCar_id() != null) {
+					log.info(" find Car SUCSESS");
+					searchResponseMeta.setCar_id(newCar.getCar_id());
+					searchResponseMeta.setImage(newCar.getImage());
+					searchResponseMeta.setCar(newCar);
+					log.info("newCar.getCar_id() = " + newCar.getCar_id());
+				} else {
+					log.info(" find Car FAILED");
+				}
+			}
+			searchResponseMeta.setColor(car.getColor());
+			searchResponseMeta.setCar_id(car.getCar_id());
+			searchResponseMeta.setYear(car.getYear());
+			searchResponseMeta.setKm(car.getKm());
+			searchResponseMeta.setPrice(car.getPrice());
+			searchResponseMeta.setType_geare(car.getType_geare());
+			searchResponseMeta.setVolume(car.getVolume());
+			User user = db.load(User.class, car.getUser_id());
+			searchResponseMeta.setUser(user);
+			searchResponseMeta.setCar(car);
+			if (car.getImage() != null) {
+				log.info("Start set Car Photo");
+				searchResponseMeta.setImage(car.getImage());
+				log.info(" set Car Photo SUCSESS");
+			}
+			searchResponseMetaList.add(searchResponseMeta);
 		}
-		log.info("End newApplication");
-		return carMetas;
+		for (SearchResponseMeta car2 : searchResponseMetaList) {
+			log.info("Car_id() = " + car2.getCar_id());
+		}
+		return searchResponseMetaList;
 	}
 
 }
